a655c8591ac989cb3443df62454940f1eeeee8f1
jkarenko <juho.karenko@gmail.com>
2025-01-26 01:18:38 +0200

    Fix(Bubble): Prevent damage when attached to player
    - Modify take_damage method to only queue_free when not attached to player
    - Add condition to sugar collision to prevent damage when attached

--- scripts/objects/Bubble.gd
+++ scripts/objects/Bubble.gd
@@ -90,14 +90,12 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 		queue_free()
 
 func _on_area_entered(area: Area2D):
-	if area.is_in_group("sugar"):
+	if area.is_in_group("sugar") and !attached_to_player:
 		take_damage()
 
 func take_damage():
 	if !attached_to_player:
 		queue_free()
-	else:
-		detach_from_player()
 
 func _exit_tree():
 	# Make sure we remove ourselves from player when being freed
