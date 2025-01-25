a94fb72c378f202f050fcf78d86920e716253ced
jkarenko <juho.karenko@gmail.com>
2025-01-25 13:48:39 +0200

    refactor(player): improve movement and camera handling
    - Increase player speed and add configurable movement limits
    - Switch to physics-based movement using `move_and_slide()`
    - Enable camera automatically in `_ready()` method
    - Simplify input handling with clearer conditional logic

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -1,18 +1,23 @@
 extends CharacterBody2D
 
-var speed = 200
+@export var speed = 400
+@export var left_limit = -1000
+@export var right_limit = 1000
 
-var left_limit = -400
-var right_limit = 400
+func _ready():
+	$Camera2D.make_current()
 
-func _process(delta):
+func _physics_process(_delta):
 	var input_direction = 0
 	
 	if Input.is_action_pressed("move_left"):
 		input_direction -= 1
-	if Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		input_direction += 1
+	else:
+		input_direction = 0
 		
-	position.x += input_direction * speed * delta
+	velocity.x = input_direction * speed
+	move_and_slide()
 	
 	position.x = clamp(position.x, left_limit, right_limit)
